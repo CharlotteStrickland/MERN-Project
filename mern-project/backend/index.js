8a9ed793966350900b7b965e2b0d10f639a5c0f3
leonardocox <leonardocx0@gmail.com>
2022-09-24 11:46:24 -0400

    Setup mongoose connection and PORT connection

--- mern-project/backend/index.js
+++ mern-project/backend/index.js
@@ -2,3 +2,27 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "20mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "20mb", extended: true }));
+
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://leonardocxMERN:863381@cluster0.gamdbwz.mongodb.net/?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 3000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Connection is established and running on port: ${PORT}`)
+    )
+  )
+  .catch((err) => console.log(err.message));
