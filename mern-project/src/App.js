3d551db8e724e629a3c3552b12b9dd310b2f646b
Charlotte Strickland <charlottemarie81@gmail.com>
2022-09-26 21:17:13 -0400

    added code to new components

--- mern-project/src/App.js
+++ mern-project/src/App.js
@@ -1,5 +1,10 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
+import React from "react";
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
