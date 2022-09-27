47cb0067b723b6d3dcfa06d7d74a011fca4202e8
Charlotte Strickland <charlottemarie81@gmail.com>
2022-09-26 22:02:13 -0400

    added search and datepicker to search.js

--- mern-project/src/components/Search.js
+++ mern-project/src/components/Search.js
@@ -1,3 +1,24 @@
 import React, { useState } from 'react';
 import './Search.css';
-import DatePicker from 
+import 'react-date-range/dist/styles.css'; 
+import { DateRangePicker } from "react-date-range";
+
+//Date Picker Component
+function Search() {
+    const [dateRange, setDateRange] = useState([null, null]);
+    const [startDate, endDate] = dateRange;
+    
+    return (
+      <DatePicker
+        selectsRange={true}
+        startDate={startDate}
+        endDate={endDate}
+        onChange={(update) => {
+          setDateRange(update);
+        }}
+        withPortal
+      />
+    )
+  }
+
+  export default Search;
